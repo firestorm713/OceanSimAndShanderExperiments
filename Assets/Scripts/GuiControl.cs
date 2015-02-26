712f7761410ea4ea3bffa50a906f208bec3d0f43
Andrew Quesenberry <andrew.quesenberry713@gmail.com>
2015-02-25 18:58:56 -0700

    Massive updates to Ocean.cs, added beginnings of wind direction controller

--- Assets/Scripts/GuiControl.cs
+++ Assets/Scripts/GuiControl.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+//[ExecuteInEditMode]
 public class GuiControl : MonoBehaviour {
 
-    float Magnitude;
+    float Choppiness;
     float Height;
     float Speed;
     float TSpeed;
     int Size;
     float Scale;
     public GameObject Ocean;
-    Plane oceanPlane;
+    Ocean oceanPlane;
 
 	// Use this for initialization
 	void Start () {
-        oceanPlane = Ocean.GetComponent<Plane>();
-        Magnitude = oceanPlane.Magnitude;
+        oceanPlane = Ocean.GetComponent<Ocean>();
+        Choppiness = oceanPlane.Choppiness;
         Height = oceanPlane.Height;
         Speed = oceanPlane.Speed;
         TSpeed = oceanPlane.TransverseSpeed;
@@ -32,10 +32,10 @@ public class GuiControl : MonoBehaviour {
     void OnGUI()
     {
         if (GUI.Button(new Rect(10, 10, 32, 32), "-"))
-            Magnitude -= 0.01f;
+            Choppiness -= 0.01f;
         if (GUI.Button(new Rect(42, 10, 32, 32), "+"))
-            Magnitude += 0.01f;
-        GUI.Box(new Rect(74, 10, 128, 32), "Magnitude = " + Magnitude);
+            Choppiness += 0.01f;
+        GUI.Box(new Rect(74, 10, 128, 32), "Choppiness = " + Choppiness);
 
         if (GUI.Button(new Rect(10, 42, 32, 32), "-"))
             Height--;//= 0.01f;
@@ -59,20 +59,31 @@ public class GuiControl : MonoBehaviour {
         }
         GUI.Box(new Rect(74, 74, 128, 32), "Speed = " + Speed);
 
-        if (GUI.Button(new Rect(10, 106, 32, 32), "-"))
-            Scale -= 0.1f;
-        if (GUI.Button(new Rect(42, 106, 32, 32), "+"))
-            Scale += 0.1f;
-        GUI.Box(new Rect(74, 106, 128, 32), "Scale = " + Scale);
-
-        if (GUI.Button(new Rect(10, 138, 32, 32), "-"))
+        //if (GUI.Button(new Rect(10, 106, 32, 32), "-"))
+        //    Scale -= 0.1f;
+        //if (GUI.Button(new Rect(42, 106, 32, 32), "+"))
+        //    Scale += 0.1f;
+        //GUI.Box(new Rect(74, 106, 128, 32), "Scale = " + Scale);
+        //
+        if (GUI.Button(new Rect(10, 138, 32, 32), "-") && Size>2)
+        {
+            Scale *= 2;
+            Size--;
             Size /= 2;
-        if (GUI.Button(new Rect(42, 138, 32, 32), "+"))
+            Size++;
+        }
+        if (GUI.Button(new Rect(42, 138, 32, 32), "+") && Size < 9)
+        {
+            Scale /= 2;
+            Size--;
             Size *= 2;
-        GUI.Box(new Rect(74, 138, 128, 32), "Size = " + Size);
+            Size++;
+        }
 
-        if (Magnitude != oceanPlane.Magnitude)
-            oceanPlane.Magnitude = Magnitude;
+        GUI.Box(new Rect(74, 138, 128, 32), "Grid Size = " + (Size-1) + "x" + (Size-1));
+
+        if (Choppiness != oceanPlane.Choppiness)
+            oceanPlane.Choppiness = Choppiness;
         if (Height != oceanPlane.Height)
             oceanPlane.Height = Height;
         if (Speed != oceanPlane.Speed)
